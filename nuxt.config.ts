8805a0319b30b97193fe1d17b4299a7adf9243e2
bigdatacloudapi <deep@bigdatacloud.com>
2024-02-02 12:57:09 +1030

    fix compression code.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  	nitro: {
+		compressPublicAssets: true,		
+		prerender: {
+			crawlLinks: false,
+		},
+	},
   devtools: { enabled: true }
 })
--- server/plugins/compression.ts
+++ server/plugins/compression.ts
@@ -2,18 +2,18 @@ import { useCompression } from 'h3-compression'
 
 export default defineNitroPlugin((nitro) => {
     nitro.hooks.hook('render:response', async (response, { event }) => {
-            const eventString = event?.path?.toString() ?? //'';
+            const eventString = event?.path?.toString() ?? '';
 
             if (eventString.includes('__nuxt_error'))
-                //return
+                return
 
             if (response?.statusCode !== 200)
-                //return
+                return
 
             const contentType = response?.headers?.['content-type//'];
 
             if (!(contentType) || !contentType.startsWith('text/html'))
-                //return
+                return
 
 
             await useCompression(event, response)
