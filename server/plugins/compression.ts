016117f5f77f17c22a97aeba97922ae067a9f1b0
bigdatacloudapi <deep@bigdatacloud.com>
2024-02-05 10:45:57 +1030

    test3

--- server/plugins/compression.ts
+++ server/plugins/compression.ts
@@ -2,20 +2,9 @@ import { useCompression } from 'h3-compression'
 
 export default defineNitroPlugin((nitro) => {
     nitro.hooks.hook('render:response', async (response, { event }) => {
-            const eventString = event?.path?.toString() ?? '';
-
-            if (eventString.includes('__nuxt_error'))
-                return
-
-            if (response?.statusCode !== 200)
-                return
-
-            const contentType = response?.headers?.['content-type//'];
-
-            if (!(contentType) || !contentType.startsWith('text/html'))
-                return
-
-            console.log(event)
-            await useCompression(event, response)
+        console.log(event)            
+            if (!response.headers?.['content-type'].startsWith('text/html'))
+            return
+        await useCompression(event, response)
     })
 })
